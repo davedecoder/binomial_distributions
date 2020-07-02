fa3cca7c933521896257b024b430c22c0f84d6b3
David I.Rock <entrinen@gmail.com>
2020-07-01 23:03:55 -0500

    Adding setup files for d_distributions package

---
+++ deep_learning/binomial/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='d_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['d_distributions'],
+      install_requires=[
+          'scipy',
+      ],
+      zip_safe=False)
