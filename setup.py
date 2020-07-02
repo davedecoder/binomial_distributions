b5abc4128af9f5be10f4261a9ed772c5211a1c04
David I.Rock <entrinen@gmail.com>
2020-07-01 23:07:16 -0500

    Bump version: 0.1.1 → 0.1.2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='py-experients',
-      version='0.1.1',
+      version='0.1.2',
       author="David Ruiz",
       author_email='entrinen@gmail.com',
       description='Python Experiments',
