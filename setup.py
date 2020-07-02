73ca5ce44671855703f06d09862ca57c91e680e3
David I.Rock <entrinen@gmail.com>
2020-07-01 22:41:47 -0500

    Adding First Commit

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='py-experients',
+      version='0.1',
+      author="David Ruiz",
+      author_email='david@kimetrics.com',
+      description='Python Experiments',
+      zip_safe=False)
