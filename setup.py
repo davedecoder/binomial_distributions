8e45c6434af97b9ce585a5c815bfe7b43622f6ec
David I.Rock <entrinen@gmail.com>
2020-07-01 22:47:02 -0500

    minor fix

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import setup
 setup(name='py-experients',
       version='0.1.0',
       author="David Ruiz",
-      author_email='david@kimetrics.com',
+      author_email='entrinen@gmail.com',
       description='Python Experiments',
       zip_safe=False)
